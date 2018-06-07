9363025a413b0385b7a36701b411f43a3617c47e
OzakiShuichiro <ozaki@giftee.co>
2018-06-07 22:26:20 +0900

    bot message interaction implemented

--- app/models/recommended_movie/inquiry/message.rb
+++ app/models/recommended_movie/inquiry/message.rb
@@ -1,11 +1,93 @@
 class RecommendedMovie::Inquiry::Message
-  def self.create(user_text)
-    if user_text.include?("オススメ")
-      movie = Movie.fetch_recommendation_movie!
-      { type: 'text', text: movie.url }
-    else
-      text = "その日、人類は思い出した。奴らに支配されていた恐怖を。鳥かごの中に囚われていた屈辱を。"
+  RECOMMENDATION_WORDS = [
+    "おすすめ",
+    "オススメ",
+    "お勧め",
+    "お薦め",
+    "オヌヌメ"
+  ]
+
+  OLD_MAN_WORDS = [
+    "今の気分はどうだ？",
+    "最近どうだ？",
+    "俺に聞くのか？",
+    "恋でもしてるのか"
+  ]
+
+  NAITO_WORDS = [
+    "内藤",
+    "ねー藤",
+    "nait",
+    "ないと",
+  ]
+
+  NETO = "ねー藤"
+  ALMIN = "アルミン"
+
+  NETO_URL = "https://filmarks.com/movies/9941"
+  NAITO_URL = "https://filmarks.com/movies/26701"
+
+  ALMIN_WORD = "その日、人類は思い出した。奴らに支配されていた恐怖を。鳥かごの中に囚われていた屈辱を。"
+
+  class << self
+    def create(user_text)
+      case
+      when recommendation_word?(user_text)
+        create_old_man_message
+      when naito_word?(user_text)
+        create_naito_message
+      when almin_word?(user_text)
+        create_almin_message
+      else
+        create_movie_recommendation_message!
+      end
+    end
+
+    private
+
+    def recommendation_word?(user_text)
+      RECOMMENDATION_WORDS.any? do |word|
+        user_text.include?(word)
+      end
+    end
+
+    def create_message(text)
       { type: 'text', text: text }
+    end
+
+    def create_old_man_message
+      create_message(OLD_MAN_WORDS.sample)
+    end
+
+    def create_naito_message(user_text)
+      if neto_word?(user_text)
+        create_message(NETO_URL)
+      else
+        create_message(NAITO_URL)
+      end
+    end
+
+    def naito_word?(user_text)
+      NAITO_WORDS.any? do |word|
+        user_text.include?(word)
+      end
+    end
+
+    def neto_word?(user_text)
+      user_text.include?(NETO)
+    end
+
+    def almin_word?(user_text)
+      user_text.include?(ALMIN)
+    end
+
+    def create_almin_message
+      create_message(ALMIN_WORD)
+    end
+
+    def create_movie_recommendation_message!
+      movie = Movie.fetch_recommendation_movie!
+      create_message(movie.url)
     end
   end
 end
